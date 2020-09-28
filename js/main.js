9f78cc3465f9704002bed24f6269483ee76b1da5
Artem <theuselessn@gmail.com>
2020-09-28 15:26:06 +0600

    createCardFragment, renderCard

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const getOrderedArray = (min, max, value, postfix) => {
   const itemsCount = getRandomNumber(min, max);
 
   for (let j = 0; j < itemsCount; j++) {
-    newArray.push(value + j + postfix);
+    newArray.push(value + (j + 1) + postfix);
   }
   return newArray;
 };
@@ -24,9 +24,9 @@ const getRandomArrayItem = (array) => {
 };
 
 // Несколько рандомных значений массива
-const getRandomArrayValues = (array) => {
+const getRandomArrayValues = (min, array) => {
   const shuffledArray = array.sort(() => 0.5 - Math.random());
-  const valuesPickedNumber = getRandomNumber(0, array.length) + 1;
+  const valuesPickedNumber = getRandomNumber(min, array.length);
   const valuesPickedArray = [];
   for (let i = 0; i < valuesPickedNumber; i++) {
     valuesPickedArray[i] = shuffledArray[i];
@@ -38,8 +38,14 @@ const getRandomArrayValues = (array) => {
 
 // Переменные
 const avatarDir = `img/avatars/user0`; // + Number + .png
-const title = `Объявление`;
+const title = `Уютное гнездышко для молодоженов №`;
 const TYPE = [`palace`, `flat`, `house`, `bungalow`];
+const TYPE_KEYS = {
+  palace: `Дворец`,
+  flat: `Квартира`,
+  house: `Дом`,
+  bungalow: `Бунгало`,
+};
 const CHECKIN = [`12:00`, `13:00`, `14:00`];
 const CHECKOUT = [`12:00`, `13:00`, `14:00`];
 const FEATURES = [
@@ -50,7 +56,7 @@ const FEATURES = [
   `elevator`,
   `conditioner`,
 ];
-const description = `Подробное описание №`;
+const description = `Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована. №`;
 const photosDir = `http://o0.github.io/assets/images/tokyo/hotel`; // + Number + .jpg
 
 const renderRandomAds = (adsNumber) => {
@@ -64,17 +70,17 @@ const renderRandomAds = (adsNumber) => {
         avatar: avatarDir + adNumber + `.png`,
       },
       offer: {
-        title: title + ` ` + adNumber,
-        address: getRandomNumber(0, 600) + ` ,` + getRandomNumber(0, 600),
+        title: title + adNumber,
+        address: getRandomNumber(0, 600) + `, ` + getRandomNumber(0, 600),
         price: getRandomNumber(100, 500),
         TYPE: TYPE[getRandomNumber(0, TYPE.length - 1)],
-        rooms: getRandomNumber(1, 5),
-        guests: getRandomNumber(1, 10),
+        rooms: getRandomNumber(2, 5),
+        guests: getRandomNumber(2, 10),
         CHECKIN: getRandomArrayItem(CHECKIN),
         CHECKOUT: getRandomArrayItem(CHECKOUT),
-        FEATURES: getRandomArrayValues(FEATURES),
+        FEATURES: getRandomArrayValues(1, FEATURES),
         description: description + adNumber,
-        photos: getOrderedArray(2, 7, photosDir, `.jpg`),
+        photos: getOrderedArray(1, 3, photosDir, `.jpg`),
       },
       location: {
         x: getRandomNumber(0, document.querySelector(`.map`).offsetWidth),
@@ -121,4 +127,83 @@ const renderPins = (adsArray) => {
   pinsArea.appendChild(pinsFragment);
 };
 
-renderPins(renderRandomAds(8));
+const fixedRenderedPins = renderRandomAds(8);
+
+renderPins(fixedRenderedPins);
+
+// module3-task2, Задание 2
+
+const cardTemplate = document
+  .querySelector(`#card`)
+  .content.querySelector(`.map__card`);
+const mapFilters = map.querySelector(`.map__filters-container`);
+
+// Рендер включенных удобств
+const renderFeatures = (featuresArr, featuresBlock, element) => {
+  featuresBlock.innerHTML = ``;
+  const featuresFragment = document.createDocumentFragment();
+
+  for (let j = 0; j < featuresArr.length; j++) {
+    const featuresItem = document.createElement(element);
+    featuresItem.classList = `map__feature map__feature--${featuresArr[j]}`;
+    featuresFragment.appendChild(featuresItem);
+  }
+
+  featuresBlock.appendChild(featuresFragment);
+};
+
+// Рендер карточки
+const createCardFragment = (adsArray) => {
+  const cardFragment = document.createDocumentFragment();
+  const newCard = cardTemplate.cloneNode(true);
+  const features = newCard.querySelector(`.popup__features`);
+  const photos = newCard.querySelector(`.popup__photos`);
+
+  for (let i = 0; i < adsArray.length; i++) {
+    /* Отрисовка только одной карточки */
+    if (i === 0) {
+      newCard.querySelector(`.popup__title`).textContent =
+        adsArray[i].offer.title;
+
+      newCard.querySelector(`.popup__text--address`).textContent =
+        adsArray[i].offer.address;
+
+      newCard.querySelector(`.popup__text--price`).textContent =
+        adsArray[i].offer.price;
+
+      newCard.querySelector(`.popup__type`).textContent =
+        TYPE_KEYS[adsArray[i].offer.TYPE];
+
+      newCard.querySelector(
+        `.popup__text--capacity`
+      ).textContent = `${adsArray[i].offer.rooms} комнаты для ${adsArray[i].offer.guests} гостей`;
+
+      newCard.querySelector(
+        `.popup__text--time`
+      ).textContent = `Заезд после ${adsArray[i].offer.CHECKIN}, выезд до ${adsArray[i].offer.CHECKOUT}`; /* Как сюда вставить &nbsp; ? =) */
+
+      renderFeatures(adsArray[i].offer.FEATURES, features, `li`);
+      newCard.querySelector(`.popup__description`).textContent =
+        adsArray[i].offer.description;
+
+      const photoTemplate = photos.children[0];
+      photos.innerHTML = ``;
+      adsArray[i].offer.photos.forEach((photoSrc) => {
+        const newPhoto = photoTemplate.cloneNode();
+        newPhoto.src = photoSrc;
+        photos.appendChild(newPhoto);
+      });
+
+      newCard.querySelector(`.popup__avatar`).src = adsArray[i].author.avatar;
+    }
+
+    cardFragment.appendChild(newCard);
+    return cardFragment;
+  }
+};
+
+const renderCard = (createFragment) => {
+  map.insertBefore(createCardFragment(fixedRenderedPins), mapFilters);
+};
+
+renderCard(createCardFragment);
